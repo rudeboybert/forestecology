3e37d2b05bb1100566d58b6d5d19db60b307c5e8
rudeboybert <albert.ys.kim@gmail.com>
2018-10-16 14:58:30 -0400

    Added new dataset

--- data-raw/process_data_raw.R
+++ data-raw/process_data_raw.R
@@ -84,3 +84,55 @@ bigwoods <- "data-raw/BigWoods2015.csv" %>%
   select(ID, species, family_phylo, trait_group, x, y, growth, dbh08, dbh14,
          code14)
 usethis::use_data(bigwoods, overwrite = TRUE)
+
+
+
+# Import NEW data
+bigwoods_new <- "data-raw/BigWoods2015_new.csv" %>%
+  read_csv() %>%
+  # Remove initially non-necessary variables
+  select(
+    -starts_with("not new"), -starts_with("notes"),
+    -c(quad, row, xsample, side, ysample, tag, stem)
+  ) %>%
+  # Note readr package reads blanks as NA's. Convert these to blank to ""
+  mutate(
+    code08 = ifelse(is.na(code08), "", code08),
+    code14 = ifelse(is.na(code14), "", code14)
+  ) %>%
+  # Define dbh and growth variables
+  mutate(
+    dbh03 = gbh03/pi,
+    dbh08 = gbh08/pi,
+    dbh14 = gbh14/pi,
+    growth = (dbh14-dbh08)/(2014-year_08)
+  ) %>%
+  # Remove those trees with year_08 = NA, i.e. were only added in 2014
+  filter(!is.na(year_08)) %>%
+  # Convert species to snake_case
+  mutate(species = snakecase::to_any_case(species)) %>%
+  # Join with families data. If no matched family_philo or trait_groud found,
+  # assign to "Misc"
+  left_join(families, by = c("species" = "spcode")) %>%
+  mutate(
+    family_phylo = ifelse(is.na(family_phylo), "Misc", family_phylo),
+    trait_group = ifelse(is.na(trait_group), "Misc", trait_group),
+  ) %>%
+  # Convert all species info to factors b/c of issue of rare levels not being
+  # included in training set, but then appearing in test set.
+  mutate(
+    species = factor(species),
+    family_phylo = factor(family_phylo),
+    trait_group = factor(trait_group)
+  ) %>%
+  # Remove 32 + 113 = 145 of 50178 that aren't strictly in interior of study
+  # region boundary polygon b/c of issues with forming crossvalidation grid
+  # later
+  mutate(inside_blocks = point.in.polygon(x, y, bigwoods_study_region$x, bigwoods_study_region$y)) %>%
+  filter(inside_blocks == 1) %>%
+  # Add ID variable
+  tibble::rownames_to_column(var = "ID") %>%
+  # Clean up variables
+  select(ID, species, family_phylo, trait_group, x, y, growth, dbh08, dbh14,
+         code14)
+usethis::use_data(bigwoods_new, overwrite = TRUE)
