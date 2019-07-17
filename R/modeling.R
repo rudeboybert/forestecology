ae945c9f653a6e05307b2f7e980c3a1dbfefb1bf
rudeboybert <albert.ys.kim@gmail.com>
2019-07-17 19:24:37 -0400

    Fixed within species shuffling

--- R/modeling.R
+++ R/modeling.R
@@ -121,19 +121,23 @@ fit_bayesian_model <- function(focal_vs_comp, model_specs, run_shuffle = FALSE,
   if(run_shuffle){
     focal_trees <- focal_trees %>%
       group_by(species) %>%
-      mutate(comp_species = sample(comp_species)) %>%
-      group_by(ID, focal_ID, species, spCode, x, y, dbh, growth, fold, comp_species)
+      mutate(comp_species = sample(comp_species))
   }
 
   # Continue processing focal_trees
   focal_trees <- focal_trees %>%
+    ungroup() %>%
+    # sum biomass and n_comp for competitors of same species. we need to do this
+    # for the cases when we do permutation shuffle.
+    group_by(ID, focal_ID, species, spCode, x, y, dbh, growth, fold, comp_species) %>%
+    summarise_all(list(sum)) %>%
+    ungroup() %>%
+    # compute biomass for each tree type
     spread(comp_species, biomass_total, fill = 0) %>%
     group_by(ID, focal_ID, species, spCode, x, y, dbh, growth, fold) %>%
-    #
-    # Hack! fix this with purrr later.
-    #
     summarise_all(list(sum)) %>%
-    filter(!is.na(growth)) %>%
+    ungroup() %>%
+    # sort by focal tree ID number
     arrange(focal_ID)
 
   # Add biomass=0 for any species for which there are no trees
@@ -237,13 +241,13 @@ predict_bayesian_model <- function(focal_vs_comp, model_specs, posterior_param){
 
   # Continue processing focal_trees
   focal_trees <- focal_trees %>%
+    ungroup() %>%
     spread(comp_species, biomass_total, fill = 0) %>%
     group_by(ID, focal_ID, species, spCode, x, y, dbh, growth, fold) %>%
     #
     # Hack! fix this with purrr later.
     #
     summarise_all(list(sum)) %>%
-    filter(!is.na(growth)) %>%
     arrange(focal_ID)
 
   # Add biomass=0 for any species for which there are no trees
